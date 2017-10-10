7283552dac6113c962feebf39f6a2d8bc1fe22e2
nishant dixit <nishantdixit24@gmail.com>
2017-10-10 17:09:55 +0530

    S

--- index.js
+++ index.js
@@ -7,64 +7,70 @@ var bodyParser = require('body-parser')
 app.set('port', (process.env.PORT || 5001))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
- console.log(fs);
-response.sendfile('./index.html');
+app.get('/', function (request, response) {
+	console.log(fs);
+	response.sendfile('./index.html');
 })
 
-app.get('/amp', function(request, response) {
- console.log(fs);
-response.sendfile('./apm.html');
+app.get('/amp', function (request, response) {
+	console.log(fs);
+	response.sendfile('./apm.html');
 })
 
-app.get('/login', function(request, response) {
- console.log(fs);
-response.sendfile('./login.html');
-  //  response.redirect(301,'http://stackoverflow.com');
-})
+app.get('/login', function (request, response) {
+	console.log(fs);
+	response.sendfile('./login.html');
+	//  response.redirect(301,'http://stackoverflow.com');
+});
 
-app.get('/:name', function(request, response) {
+//app.get('/:name', function(request, response) {
+//
+//    var options = {
+//        root: __dirname + '/',
+//        headers: {
+//            'x-timestamp': Date.now(),
+//            'x-sent': true
+//        }
+//    }; 
+//
+//    var fileName = request.params.name;
+//    response.sendFile(fileName, options, function (err) {
+//        if (err) {
+//            console.log(err);
+//        } else {
+//            console.log('Sent:', fileName);
+//        }
+//    });
+//})
 
-    var options = {
-        root: __dirname + '/',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    }; 
+app.post("/sendnotification", function (req, res) {
 
-    var fileName = request.params.name;
-    response.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-})
+	webPush.sendNotification(req.body.endpoint, {
+			payload: JSON.stringify({
+				'title': req.body.title,
+				'icon': req.body.icon,
+				'body': req.body.body,
+				'url': req.body.link
+			}),
+			userPublicKey: req.body.key,
+			userAuth: req.body.authSecret,
+		})
+		.then(function () {
+			console.log("sent push")
+			res.send({
+				"status": "sucess"
+			});
+		}, function (err) {
+			console.log('webpusherr', err);
+			res.send({
+				"status": "error",
+				"error": err
+			});
+		});
 
-app.post("/send/push",function(request,response){
- webPush.sendNotification(req.body.endpoint, {
-      payload: JSON.stringify({
-        'title': req.body.title,
-        'icon': req.body.icon,
-        'body': req.body.body,
-        'url': req.body.link
-      }),
-      userPublicKey: req.body.key,
-      userAuth: req.body.authSecret,
-    })
-    .then(function() {
-      console.log("sent push")
-    res.send({"status":"sucess"});
-    }, function(err) {
-      console.log('webpusherr', err);
-res.send({"status":"error","error":err});
-    });
-	
 });
 
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function () {
+	console.log("Node app is running at localhost:" + app.get('port'))
 })
