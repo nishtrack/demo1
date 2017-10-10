f468929ccbf68e84a5548e1ec4fc24a53224b49e
nishant dixit <nishantdixit24@gmail.com>
2017-10-10 18:08:15 +0530

    c

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 var express = require('express')
-var app = express()
+var app = express();
 var fs = express("fs");
 var webpush = require('web-push');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(bodyParser.json());
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
+app.use(cors());
 
 webpush.setGCMAPIKey('AIzaSyBgtX_-6hCWfshS4xyUVq-pNPYEM0GXNGo');
 
