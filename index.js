27db605ed04b818e28bfd835dff2c164e6fe54d1
nishant dixit <nishantdixit24@gmail.com>
2017-10-11 11:11:16 +0530

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://test-a1f74.firebaseapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
